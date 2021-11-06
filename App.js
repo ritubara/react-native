dc9bb54d515ec40c7fc2ab8ad8b841cdd90dd890
Ritu Bara <ritu.bara78@gmail.com>
2021-11-06 09:28:53 -0500

    second update

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Button from "./home";
+
+export default class App extends React.Component {
+  // state = {
+  //   myState:
+  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, used do eiusmod fgfhgkjljl",
+  // };
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <Text> {this.state.myState} </Text> */}
+        <Button />
+      </View>
+    );
+  }
 }
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>change{2 + 2}</Text>
+//       {/* <StatusBar style="auto" /> */}
+//     </View>
+//   );
+// }
+
+// return (
+//   <View style={styles.container}>
+//     <Text>Open up App.js to start working on your app!</Text>
+//     {/* <StatusBar style="auto" /> */}
+//   </View>
+// );
+
+// return (
+//   <View style={styles.container}>
+//     <Text>change{2 + 2}</Text>
+//     {/* <StatusBar style="auto" /> */}
+//   </View>
+// );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+class Button extends Component {
+  // state = {
+  //    text: 'Failure'
+  // }
+
+  // updateButtonText = () ⇒ this.setState({ text: 'Success' })
+
+  render() {
+    return (
+      <View>
+        <Text>hello to login hgjlj</Text>
+      </View>
+    );
+  }
+}
+export default Button;
